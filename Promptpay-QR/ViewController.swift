7f849ce4ff97c8d32a9aed51e19c0c848224e4d5
Issara Boonyamoon <issara.boonyamoon@gmail.com>
2017-09-18 20:49:37 +0700

    Initial Commit

---
+++ Promptpay-QR/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Promptpay-QR
+//
+//  Created by Issara Boonyamoon on 9/18/17.
+//  Copyright © 2017 Issara Boonyamoon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
